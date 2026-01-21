859636df097f16375a3060b9b01aad4692156434
AnalystYuchels <uche4xploit@gmail.com>
2026-01-21 17:31:24 +0100

    Fix undefined bandName error in EJS template

--- Band Generator Project/index.js
+++ Band Generator Project/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 // View engine
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
 
 // Static files
 app.use(express.static("public"));
@@ -19,7 +19,9 @@ app.use((req, res, next) => {
 
 //Home route
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    bandName: null
+  });
 });
 
 // Generate band name
