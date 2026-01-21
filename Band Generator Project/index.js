fffbe6bd659e3d7c3ba36215b9866ce83c4e3ca1
AnalystYuchels <uche4xploit@gmail.com>
2026-01-21 17:26:20 +0100

    Added code for Band name generator and animated rendering

--- Band Generator Project/index.js
+++ Band Generator Project/index.js
@@ -4,34 +4,39 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-//Step 3 - Make the styling show up.
-//Hint 1: CSS files are static files!
-//Hint 2: The header and footer are partials.
-//Hint 3: Add the CSS link in header.ejs
+// View engine
+app.set("view engine", ejs);
 
-//Step 4 - Add a dynamic year to the footer.
-//Hint: Google to find out how to get the current year using JS.
-
+// Static files
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
+// Global year middleware
+app.use((req, res, next) => {
+  res.locals.year = new Date().getFullYear();
+  next();
 });
 
+//Home route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Generate band name
 app.post("/submit", (req, res) => {
-  //Step 2 - Make the generate name functionality work
-  //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
-  //Then:
-  //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
-  //scroll down to see the two arrays.
-  //2. Send the index.ejs as a response and add the adjective and noun to the res.render
-  //3. Test to make sure that the random words display in the h1 element in index.ejs
+  const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  const randomNoun = noun[Math.flooor(Math.random() * noun.length)];
+
+  res.render("index", {
+    bandName: `${randomAdj} ${randomNoun}`,
+  });
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+// Word banks
 const adj = [
   "abandoned",
   "able",
